b5d91a246c9b00782e1652677d4215129213a5c4
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-08-21 11:14:10 -0600

    First steps: Add blogpost list to /post path

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Mini Blog")
 
+BLOG_POST = [
+    {"id": 1, "title": "Hola desde FastAPI", "Content":"Mi primer post con FastAPI"},
+    {"id": 2, "title": "Mi segundo Post con FastAPI", "Content":"Mi segundo post con FastAPI blablabla"},
+    {"id": 3, "title": "Django vs FastAPI", "Content":"FastAPI es más rápido por x razones"},
+]
+
+
 @app.get("/")
 def home():
     return {'message': 'Bienvenidos a Mini Blog por Devtalles'}
+
+
+@app.get("/posts")
+def list_posts():
+    return {"data": BLOG_POST}
