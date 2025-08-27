fa5cdaa40cd995fd8701a0e17504430c10a943be
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-08-26 21:23:56 -0600

    Params: Add Path and Query params validations

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 
-from fastapi import FastAPI, Query, Body, HTTPException
+from fastapi import FastAPI, Query, Body, HTTPException, Path
 from pydantic import BaseModel, Field, field_validator, EmailStr
 from typing import Optional, List, Union
 
 app = FastAPI(title="Mini Blog")
 
 BLOG_POST = [
-    {"id": 1, "title": "Hola desde FastAPI", "content":"Mi primer post con FastAPI"},
-    {"id": 2, "title": "Mi segundo Post con FastAPI", "content":"Mi segundo post con FastAPI blablabla"},
-    {"id": 3, "title": "Django vs FastAPI", "content":"FastAPI es más rápido por x razones"},
+    {"id": 1, "title": "Hola desde FastAPI",
+        "content": "Mi primer post con FastAPI"},
+    {"id": 2, "title": "Mi segundo Post con FastAPI",
+        "content": "Mi segundo post con FastAPI blablabla"},
+    {"id": 3, "title": "Django vs FastAPI",
+        "content": "FastAPI es más rápido por x razones"},
 ]
 
+
 class Tag(BaseModel):
-    name: str = Field(..., min_length=2, max_length=30, description="Nombre de la etiqueta")
+    name: str = Field(..., min_length=2, max_length=30,
+                      description="Nombre de la etiqueta")
+
 
 class Author(BaseModel):
     name: str
     email: EmailStr
 
+
 class PostBase(BaseModel):
     title: str
     content: str
-    tags: Optional[List[Tag]] = []
+    tags: Optional[List[Tag]] = Field(default_factory=list)  # []
     author: Optional[Author] = None
-    
+
+
 class PostCreate(BaseModel):
     title: str = Field(
         ...,
@@ -38,25 +46,28 @@ class PostCreate(BaseModel):
         description="Contenido del post (mínimo 10 caracteres)",
         examples=["Este es un contenido válido porque tiene 10 caracteres o más"]
     )
-    tags: List[Tag] = []
+    tags: List[Tag] = Field(default_factory=list)  # []
     author: Optional[Author] = None
-    
+
     @field_validator("title")
     @classmethod
-    def not_allowed_title(cls, value:str) -> str:
+    def not_allowed_title(cls, value: str) -> str:
         if "spam" in value.lower():
             raise ValueError("El título no puede contener la palabra: 'spam'")
         return value
 
+
 class PostUpdate(BaseModel):
-    title: str
+    title: Optional[str] = Field(None, min_length=3, max_length=100)
     content: Optional[str] = None
+
 
 class PostPublic(PostBase):
     id: int
-    
+
+
 class PostSummary(BaseModel):
-    id:int
+    id: int
     title: str
 
 
@@ -66,30 +77,44 @@ def home():
 
 
 @app.get("/posts", response_model=List[PostPublic])
-def list_posts(query: str | None = Query(default=None, description="Texto para buscar por título")):
-    
+def list_posts(query: Optional[str] = Query(
+    default=None,
+    description="Texto para buscar por título",
+    alias="search",
+    min_length=3,
+    max_length=50,
+    pattern=r"^[\w\sáéíóúÁÉÍÓÚüÜ-]+$"
+)):
+
     if query:
-        return [post for post in BLOG_POST if query.lower() in  post["title"].lower() ]
-    
+        return [post for post in BLOG_POST if query.lower() in post["title"].lower()]
+
     return BLOG_POST
- 
- 
+
+
 @app.get("/posts/{post_id}", response_model=Union[PostPublic, PostSummary], response_description="Post encontrado")
-def get_post(post_id: int, include_content: bool = Query(default=True, description="Incluir o no el contenido")):
+def get_post(post_id: int = Path(
+    ...,
+    ge=1,
+    title="ID del post",
+    description="Identificador entero del post. Debe ser mayor a 1",
+    example=1
+), include_content: bool = Query(default=True, description="Incluir o no el contenido")):
     for post in BLOG_POST:
         if post["id"] == post_id:
             if not include_content:
                 return {"id": post["id"], "title": post["title"]}
             return post
-    
+
     return HTTPException(status_code=404, detail="Post no encontrado")
+
 
 @app.post("/posts", response_model=PostPublic, response_description="Post creado (OK)")
 def create_post(post: PostCreate):
     new_id = (BLOG_POST[-1]["id"]+1) if BLOG_POST else 1
-    new_post = {"id": new_id, 
-                "title": post.title, 
-                "content": post.content, 
+    new_post = {"id": new_id,
+                "title": post.title,
+                "content": post.content,
                 "tags": [tag.model_dump() for tag in post.tags],
                 "author": post.author.model_dump() if post.author else None
                 }
@@ -101,11 +126,14 @@ def create_post(post: PostCreate):
 def update_post(post_id: int, data: PostUpdate):
     for post in BLOG_POST:
         if post["id"] == post_id:
-            playload = data.model_dump(exclude_unset=True) # {"title": "Ricardo", "content": None}
-            if "title" in playload: post["title"] = playload["title"]
-            if "content" in playload: post["content"] = playload["content"]
+            # {"title": "Ricardo", "content": None}
+            playload = data.model_dump(exclude_unset=True)
+            if "title" in playload:
+                post["title"] = playload["title"]
+            if "content" in playload:
+                post["content"] = playload["content"]
             return post
-    
+
     raise HTTPException(status_code=404, detail="Post no encontrado")
 
 
@@ -115,4 +143,4 @@ def delete_post(post_id: int):
         if post["id"] == post_id:
             BLOG_POST.pop(index)
             return
-    raise HTTPException(status_code=404,detail="Post no encontrado")
+    raise HTTPException(status_code=404, detail="Post no encontrado")
