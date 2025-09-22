0b17461af67fab055189052745c97f018040ec47
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-09-22 14:05:10 -0600

    Modularization: Add schemas

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 
-import os
-from datetime import datetime
+
 from fastapi import FastAPI, Query, Body, HTTPException, Path, status, Depends
 from pydantic import BaseModel, Field, field_validator, EmailStr, ConfigDict
 from typing import Optional, List, Union, Literal
@@ -11,168 +10,12 @@ from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
-
-
-engine_kwargs = {}
-if DATABASE_URL.startswith("sqlite"):
-    engine_kwargs["connect_args"] = {"check_same_thread": False}
-
-engine = create_engine(DATABASE_URL, echo=True, future=True, **engine_kwargs)
-
-SessionLocal = sessionmaker(
-    bind=engine, autoflush=False, autocommit=False, class_=Session)
-
-
-class Base(DeclarativeBase):
-    pass
-
-
-post_tags = Table(
-    "post_tags",
-    Base.metadata,
-    Column("post_id", ForeignKey(
-        "posts.id", ondelete="CASCADE"), primary_key=True),
-    Column("tag_id", ForeignKey("tags.id", ondelete="CASCADE"), primary_key=True)
-)
-
-
-class AuthorORM(Base):
-    __tablename__ = "authors"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
-    email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
-
-    posts: Mapped[List["PostORM"]] = relationship(back_populates="author")
-
-
-class TagORM(Base):
-    __tablename__ = "tags"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String(30), unique=True, index=True)
-
-    posts: Mapped[List["PostORM"]] = relationship(
-        secondary=post_tags,
-        back_populates="tags",
-        lazy="selectin"
-    )
-
-
-class PostORM(Base):
-    __tablename__ = "posts"
-    __table_args__ = (UniqueConstraint("title", name="unique_post_title"),)
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    title: Mapped[str] = mapped_column(String(100), nullable=False, index=True)
-    content: Mapped[str] = mapped_column(Text, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime, default=datetime.utcnow)
-
-    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("authors.id"))
-    author: Mapped[Optional["AuthorORM"]] = relationship(
-        back_populates="posts")
-
-    tags: Mapped[List["TagORM"]] = relationship(
-        secondary=post_tags,
-        back_populates="posts",
-        lazy="selectin",
-        passive_deletes=True
-    )
 
 
 Base.metadata.create_all(bind=engine)  # dev
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 app = FastAPI(title="Mini Blog")
-
-
-class Tag(BaseModel):
-    name: str = Field(..., min_length=2, max_length=30,
-                      description="Nombre de la etiqueta")
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class Author(BaseModel):
-    name: str
-    email: EmailStr
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    tags: Optional[List[Tag]] = Field(default_factory=list)  # []
-    author: Optional[Author] = None
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class PostCreate(BaseModel):
-    title: str = Field(
-        ...,
-        min_length=3,
-        max_length=100,
-        description="Titulo del post (mínimo 3 caracteres, máximo 100)",
-        examples=["Mi primer post con FastAPI"]
-    )
-    content: Optional[str] = Field(
-        default="Contenido no disponible",
-        min_length=10,
-        description="Contenido del post (mínimo 10 caracteres)",
-        examples=["Este es un contenido válido porque tiene 10 caracteres o más"]
-    )
-    tags: List[Tag] = Field(default_factory=list)  # []
-    author: Optional[Author] = None
-
-    @field_validator("title")
-    @classmethod
-    def not_allowed_title(cls, value: str) -> str:
-        if "spam" in value.lower():
-            raise ValueError("El título no puede contener la palabra: 'spam'")
-        return value
-
-
-class PostUpdate(BaseModel):
-    title: Optional[str] = Field(None, min_length=3, max_length=100)
-    content: Optional[str] = None
-
-
-class PostPublic(PostBase):
-    id: int
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class PostSummary(BaseModel):
-    id: int
-    title: str
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class PaginatedPost(BaseModel):
-    page: int
-    per_page: int
-    total: int
-    total_pages: int
-    has_prev: bool
-    has_next: bool
-    order_by: Literal["id", "title"]
-    direction: Literal["asc", "desc"]
-    search: Optional[str] = None
-    items: List[PostPublic]
 
 
 @app.get("/")
