751601ce5c5a904916fba95e77af87661d8cbd47
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-09-27 14:15:15 -0600

    DepsAuth: Separate raise functions

--- app/core/security.py
+++ app/core/security.py
@@ -13,6 +13,27 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
 
+credentials_exc = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="No autenticado",
+    headers={"WWW-Authenticate": "Bearer"}
+)
+
+
+def raise_expired_token():
+    return HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token expirado",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
+
+def raise_frobidden():
+    return HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="No tienes permisos suficientes",
+    )
+
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
@@ -30,12 +51,6 @@ def decode_token(token: str) -> dict:
 
 async def get_current_user(token: str = Depends(oauth2_scheme)) -> dict:
 
-    credentials_exc = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="No autenticado",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-
     try:
         playload = decode_token(token)
         sub: Optional[str] = playload.get("sub")
@@ -45,10 +60,6 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> dict:
 
         return {"email": sub, "username": username}
     except ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token expirado",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
+        raise raise_expired_token()
     except InvalidTokenError:
         raise credentials_exc
