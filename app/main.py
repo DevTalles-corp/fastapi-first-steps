ecdfd1d052c17b878d0150872d0b70c131326f4f
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-10-13 14:50:36 -0600

    TagsUsers: Add create tag endpoint

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from app.api.v1.posts.router import router as post_router
 from app.api.v1.auth.router import router as auth_router
 from app.api.v1.uploads.router import router as upload_router
+from app.api.v1.tags.router import router as tag_router
 from fastapi.staticfiles import StaticFiles
 
 load_dotenv()
@@ -18,6 +19,7 @@ def create_app() -> FastAPI:
     Base.metadata.create_all(bind=engine)  # dev
     app.include_router(auth_router, prefix="/api/v1")
     app.include_router(post_router)
+    app.include_router(tag_router)
     app.include_router(upload_router)
 
     os.makedirs(MEDIA_DIR, exist_ok=True)
